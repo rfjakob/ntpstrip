15d5feef935be5d0a2d35447463a9834cd2ce37b
Jakob Unterwurzacher <jakobunt@gmail.com>
2024-06-25 20:10:52 +0200

    config_example.py: fix typo in DOTCOLOR
    This was bluer than indented.

--- config_example.py
+++ config_example.py
@@ -3,7 +3,7 @@ WIFI_PASSWORD = "sg44e54!!vgsd"
 
 PIXELS = 144  # The number of pixels on the neopixel strip
 GPIOPIN = 15  # The pin that the signal wire of the LED strip is connected to
-DOTCOLOR = (0, 10, 19)  # Color of the time dot
+DOTCOLOR = (0, 10, 10)  # Color of the time dot
 
 # POSIX Timezone String. Pick one from here:
 # https://github.com/nayarsystems/posix_tz_db/blob/master/zones.json
